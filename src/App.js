3543a46ac8d4d1c4da547459fa87dc42b75dfc32
tauri_s <tauristclaire@gmail.com>
2023-12-18 16:34:42 -0800

    Add notes to explain function to update state for window size

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ is outside of the scope of your component. To start, the number
 of the screen size doesn't change*/
 const App = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  /*1. Attach an event listener to the window object*/
   useEffect(() => {
+    /*2. Add function to update state of window size */
     const windowResize = () => {
       setWindowWidth(window.innerWidth)
     }
+    /*1. Attach an event listener to the window object*/
     window.addEventListener("resize", windowResize);
   }, [])
 
