8da42d747eb0da9eae953c7fcd16b579c956300b
tauri_s <tauristclaire@gmail.com>
2023-12-18 16:05:20 -0800

    Add comment to set parameters for exercise

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./styles.css";
 import { useState, useEffect } from "react";
-
+/*Use useEffect to manage a side effect (window size) that
+is outside of the scope of your component. To start, the number
+of the screen size doesn't change*/
 const App = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
