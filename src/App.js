5420212e40cd326204b0a1579b1cb1bbf10906a3
tauri_s <tauristclaire@gmail.com>
2023-12-18 16:14:41 -0800

    Add useEffect function with event listener that listens to screen resize

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ is outside of the scope of your component. To start, the number
 of the screen size doesn't change*/
 const App = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  /*1. Attach an event listener to the window object*/
+  useEffect(() => {
+    window.addEventListener("resize", windowResize);
+  }, [])
 
   return (
     <div className="App">
